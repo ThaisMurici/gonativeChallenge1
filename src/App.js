63c52b986d02694c8aad1174569d7277974feba2
Thais Moraes <murici.thais@gmail.com>
2019-03-18 15:26:01 -0300

    Configuring Reactotron

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import ReactotronConfig from './config/ReactrotronConfig';
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
