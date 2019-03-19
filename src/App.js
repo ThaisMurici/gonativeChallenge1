9ad1809d057c34dc2b6e530247b16cb5f56d1ac0
Thais Moraes <murici.thais@gmail.com>
2019-03-19 16:03:02 -0300

    Adding posts feed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet, ScrollView, Text, View, Platform, StatusBar,
+} from 'react-native';
 
 import ReactotronConfig from './config/ReactrotronConfig';
+import Post from '~/components/Post';
+
+export default class App extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        author: 'Thais Moraes',
+        title: 'Learning React Native',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo tellus eu facilisis mattis. Donec quam urna, suscipit sed euismod ac, tincidunt eu erat. Nullam finibus aliquet sodales. Proin eget semper urna. Sed in tempus eros, cursus volutpat metus. Fusce placerat, lacus at finibus varius, arcu sapien lacinia mauris, facilisis tincidunt ipsum mauris sed magna. Phasellus eu tortor justo.',
+      },
+      {
+        id: 2,
+        author: 'Thais Moraes',
+        title: 'Learning React Native',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo tellus eu facilisis mattis. Donec quam urna, suscipit sed euismod ac, tincidunt eu erat. Nullam finibus aliquet sodales. Proin eget semper urna. Sed in tempus eros, cursus volutpat metus. Fusce placerat, lacus at finibus varius, arcu sapien lacinia mauris, facilisis tincidunt ipsum mauris sed magna. Phasellus eu tortor justo.',
+      },
+      {
+        id: 3,
+        author: 'Thais Moraes',
+        title: 'Learning React Native',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo tellus eu facilisis mattis. Donec quam urna, suscipit sed euismod ac, tincidunt eu erat. Nullam finibus aliquet sodales. Proin eget semper urna. Sed in tempus eros, cursus volutpat metus. Fusce placerat, lacus at finibus varius, arcu sapien lacinia mauris, facilisis tincidunt ipsum mauris sed magna. Phasellus eu tortor justo.',
+      },
+      {
+        id: 4,
+        author: 'Thais Moraes',
+        title: 'Learning React Native',
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean commodo tellus eu facilisis mattis. Donec quam urna, suscipit sed euismod ac, tincidunt eu erat. Nullam finibus aliquet sodales. Proin eget semper urna. Sed in tempus eros, cursus volutpat metus. Fusce placerat, lacus at finibus varius, arcu sapien lacinia mauris, facilisis tincidunt ipsum mauris sed magna. Phasellus eu tortor justo.',
+      },
+    ],
+  };
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar style={styles.status} />
+        <View style={styles.header}>
+          <Text style={styles.title}>GoNative App</Text>
+        </View>
+        <ScrollView style={styles.feed}>
+          {posts && posts.map(post => <Post key={post.id} postData={post} />)}
+        </ScrollView>
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#7e57c2',
   },
-  welcome: {
-    fontSize: 20,
+  header: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 3,
+    ...Platform.select({
+      ios: {
+        height: 86,
+        paddingTop: 30,
+      },
+      android: {
+        height: 56,
+        paddingTop: 0,
+      },
+    }),
+  },
+  title: {
+    fontSize: 16,
+    fontWeight: 'bold',
     textAlign: 'center',
     margin: 10,
   },
+  feed: {
+    flex: 1,
+    padding: 15,
+  },
+  status: {
+    backgroundColor: '#fff',
+  },
 });
-
-const App = () => (
-  <View style={styles.container}>
-    <Text style={styles.welcome}>Welcome to React Native!</Text>
-  </View>
-);
-
-export default App;
---
+++ src/components/Post.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import PropTypes from 'prop-types';
+
+const Post = ({ postData }) => (
+  <View style={styles.container}>
+    <View style={styles.header}>
+      <Text style={styles.title}>{postData.title}</Text>
+      <Text style={styles.author}>{postData.author}</Text>
+    </View>
+    <Text>{postData.content}</Text>
+  </View>
+);
+
+Post.propTypes = {
+  postData: PropTypes.shape({
+    author: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    content: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default Post;
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    padding: 15,
+    marginBottom: 15,
+    borderRadius: 5,
+  },
+  header: {
+    borderBottomWidth: 1,
+    borderBottomColor: '#d3d3d3',
+    marginBottom: 5,
+    paddingBottom: 5,
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 14,
+    marginBottom: 3,
+  },
+  author: {
+    fontSize: 12,
+    color: '#939393',
+  },
+});
